e1dfea3d3d369523a91ccf290eaa3350c4ae0906
nils charlois <Nils.Charlois@clanwilliamhealth.com>
2024-06-25 13:39:45 +0100

    Added cool navbar to start structuring the UI

---
+++ scrimba_part_2/src/Components/NavbarComponent.jsx
@@ -0,0 +1,30 @@
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import logo from '../assets/react.svg'
+import logoGroceryStore from '../assets/tqt8v2cj0q3b2gqq9l4u150d72.png'
+
+export function NavbarComponent(){
+    return (
+    <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
+        <Navbar.Brand>
+            Grocery planner {''}
+          <img src={logoGroceryStore} width="50px" height="50px" />
+        </Navbar.Brand>
+        <Navbar.Toggle className="coloring" />
+        <Navbar.Collapse>
+          <Nav>
+            <NavDropdown title="Products">
+              <NavDropdown.Item href="#products/tea">Tea</NavDropdown.Item>
+              <NavDropdown.Item href="#products/coffee">Coffee</NavDropdown.Item>
+              <NavDropdown.Item href="#products/chocolate">Chocolate</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#products/promo">Promo</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link href="#about-us">About Us</Nav.Link>
+            <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+    )
+}
