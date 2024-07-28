f236e25219c871f42f54eb62d5a450c84695734a
nilscharlois <34681084+NilsCharlois@users.noreply.github.com>
2024-07-28 16:39:25 +0000

    Added MemeData

--- scrimba_part_3/src/Components/MemeComponent.jsx
+++ scrimba_part_3/src/Components/MemeComponent.jsx
@@ -1,32 +1,43 @@
+import AllMemes from "./AllMemes";
+import React from "react";
+
 export function MemeComponent() {
-    return (
-<main>
-    <form className="form">
-    <div>
-        <label htmlFor="toptext">Top Text</label>
-        <input
+  const [currentUrl, setCurrentUrl] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = AllMemes.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setCurrentUrl(url);
+    console.log(url);
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <div>
+          <label htmlFor="toptext">Top Text</label>
+          <input
             id="toptext"
             type="text"
             placeholder="Shut up"
             className="form--input"
-        />
-    </div>
-    <div>
-        <label htmlFor="bottomtext">Bottom Text</label>
-        <input
+          />
+        </div>
+        <div>
+          <label htmlFor="bottomtext">Bottom Text</label>
+          <input
             id="bottomtext"
             type="text"
             placeholder="and take my money"
             className="form--input"
-        />
-    </div>
-    <button
-        className="form--button"
-    >
-        Get a new meme image 🖼
-    </button>
-    </form>
-</main>
-
-    )
+          />
+        </div>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+        <img className="meme--image" alt="image" src={currentUrl} />
+      </div>
+    </main>
+  );
 }
